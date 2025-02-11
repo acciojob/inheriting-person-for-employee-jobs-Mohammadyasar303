e4cdee0d7cba310e501e732aac08de8593f209e0
Mohammadyasar Mansuri <126454357+Mohammadyasar303@users.noreply.github.com>
2025-02-11 11:02:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,8 +8,8 @@
 
 class Person {
 	constructor(name, age) {
-		this.name=name;
-		this.age=age;
+		this.name=string;
+		this.age=number;
 	}
 	get greet() {
     return `Hello, my name is ${this.name}, I am ${this.age} years old`;
